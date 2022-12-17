018d0f7c1d726558224fc8696cb5bb443fa8ce7f
anikobakhidze <kobakhidzeana23@gmail.com>
2022-12-18 01:43:29 +0400

    add js file

---
+++ index.js
@@ -0,0 +1,8 @@
+var lastName = "kobakhidze",
+  firstName = "anna",
+  age = 26;
+if (lastName !== "" && firstName !== "" && age > 18) {
+  console.log("user is valid");
+} else {
+  console.log("user is invalid");
+}
